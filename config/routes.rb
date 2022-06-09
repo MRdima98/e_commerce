5a97903baf9cbdd5b116f02aac141fac8cd8252b
MRdima98 <dimafrunza69@gmail.com>
2022-06-09 15:31:53 +0200

    home page_set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "home_page#index"
+  get "/home_page", to: "home_page#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
