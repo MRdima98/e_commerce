78181d4f8e39b9fbe6809a642353afa06012cec9
MRdima98 <dimafrunza69@gmail.com>
2022-06-10 09:35:54 +0200

    added home page and login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "home_page#index"
   get "/home_page", to: "home_page#index"
+  get "/login", to: "login#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
